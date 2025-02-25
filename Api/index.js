f89cb890f23b70f9549843790b5a907eb2250778
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-02-25 08:37:17 -0800

    fixing my server error

--- Api/index.js
+++ Api/index.js
@@ -4,10 +4,12 @@ import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
 import path from 'path';
 
+
 // Import routes
 import userRouter from "./routes/user.route.js"
 import authRouter from "./routes/auth.route.js"
 import listingRouter from "./routes/listing.route.js"
+const port = process.env.PORT || 3000
 
 
 dotenv.config();
@@ -27,18 +29,12 @@ app.use(express.json())
 app.use(cookieParser())
 
 
-app.listen(3000, () =>{
-    console.log('Server is running on port 3000');
-})
 
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
-})
+
 
 app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
@@ -49,6 +45,28 @@ app.use((err, req, res, next) =>{
         statusCode,
     })
 })
+//checking if the process.env.NODE_ENV === 'production'
+
+if (process.env.NODE_ENV === "production") {
+    // Serve static assets from the client/build folder in production mode
+    app.use(express.static(path.join(__dirname, "/frontend/dist")));
+  
+    // any route that is not api will be redirected to index.html
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+    });
+  } else {
+    // Route to test if the API is running
+    app.get("/", (req, res) => {
+      res.send("Hello, World! My API is running");
+    });
+  }
+  
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
 
 
 
