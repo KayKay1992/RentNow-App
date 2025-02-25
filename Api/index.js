534a3b1ceb92075991669478594c75b14e9fb385
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-02-25 07:17:14 -0800

    Setting my server before deploying to render

--- Api/index.js
+++ Api/index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from "dotenv";
 import cookieParser from 'cookie-parser';
+import path from 'path';
 
 // Import routes
 import userRouter from "./routes/user.route.js"
@@ -10,17 +11,21 @@ import listingRouter from "./routes/listing.route.js"
 
 
 dotenv.config();
-const app = express();
-
-app.use(express.json())
-
-app.use(cookieParser())
 
 mongoose.connect(process.env.MONGO_URI).then(()=> {
     console.log('Connected to MongoDB');
 }).catch((err) => {
     console.log(err)
 })
+
+const __dirname = path.resolve()
+
+const app = express();
+
+app.use(express.json())
+
+app.use(cookieParser())
+
 
 app.listen(3000, () =>{
     console.log('Server is running on port 3000');
@@ -29,6 +34,11 @@ app.listen(3000, () =>{
 app.use("/api/user", userRouter)
 app.use("/api/auth", authRouter)
 app.use("/api/listing", listingRouter)
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err, req, res, next) =>{
     const statusCode = err.statusCode || 500;
