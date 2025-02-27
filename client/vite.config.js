83659eae4e69cf6f46bae118a93dca18a7c07dec
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-02-27 02:16:50 -0800

    ii

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,15 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   server: {
-    proxy:{
+    proxy: {
       '/api': {
         target: 'http://localhost:3000',
         secure: false,
-      }
-    }
+      },
+    },
   },
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // Specify the output directory
+    chunkSizeWarningLimit: 1000, // Optional: Adjust chunk size warning limit
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'], // Optional: Split vendor chunks
+        },
+      },
+    },
+  },
+});
