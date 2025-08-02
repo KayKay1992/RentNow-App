0aa3e92ea5a2e33fa210622f9e91a37ef09013ec
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-08-02 10:24:44 -0700

    vite.config update

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,11 +2,13 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  server: {
+   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://rent-now-app-r2qe.vercel.app' ,
+        changeOrigin: true,
         secure: false,
+        rewrite: (path) => path.replace(/^\/api/, ''),
       },
     },
   },
