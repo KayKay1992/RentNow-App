33fd14af0931121ff08d36c5647167bbd93bb8c5
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-08-05 06:43:04 -0700

    optimization

--- Api/index.js
+++ Api/index.js
@@ -1,82 +1,3 @@
-// import express from 'express';
-// import mongoose from 'mongoose';
-// import dotenv from "dotenv";
-// import cookieParser from 'cookie-parser';
-// import path from 'path';
-
-// // Import routes
-// import userRouter from "./routes/user.route.js"
-// import authRouter from "./routes/auth.route.js"
-// import listingRouter from "./routes/listing.route.js"
-// const port = process.env.PORT || 3000
-
-// dotenv.config();
-
-// mongoose.connect(process.env.MONGO_URI).then(()=> {
-//     console.log('Connected to MongoDB');
-// }).catch((err) => {
-//     console.log(err)
-// })
-
-// const __dirname = path.resolve()
-
-// const app = express();
-
-// app.use(express.json())
-
-// app.use(cookieParser())
-
-// app.use("/api/user", userRouter)
-// app.use("/api/auth", authRouter)
-// app.use("/api/listing", listingRouter)
-
-// //checking if the process.env.NODE_ENV === 'production'
-
-// if (process.env.NODE_ENV === "production") {
-//     // Serve static assets from the client/build folder in production mode
-//     app.use(express.static(path.join(__dirname, "/client/dist")));
-
-//     // any route that is not api will be redirected to index.html
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
-//     });
-//   } else {
-//     // Route to test if the API is running
-//     app.get("/", (req, res) => {
-//       res.send("Hello, World! My API is running");
-//     });
-//   }
-
-// app.use((err, req, res, next) =>{
-//     const statusCode = err.statusCode || 500;
-//     const message = err.message || 'Internal Server Error'
-//     return res.status(statusCode).json({
-//         success: false,
-//         message,
-//         statusCode,
-//     })
-// })
-// //checking if the process.env.NODE_ENV === 'production'
-
-// if (process.env.NODE_ENV === "production") {
-//     // Serve static assets from the client/build folder in production mode
-//     app.use(express.static(path.join(__dirname, "/frontend/dist")));
-
-//     // any route that is not api will be redirected to index.html
-//     app.get("*", (req, res) => {
-//       res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-//     });
-//   } else {
-//     // Route to test if the API is running
-//     app.get("/", (req, res) => {
-//       res.send("Hello, World! My API is running");
-//     });
-//   }
-
-// app.listen(port, () => {
-//     console.log(`Server is running on port ${port}`);
-// });
-
 import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
@@ -88,15 +9,9 @@ import userRouter from "./routes/user.route.js";
 import authRouter from "./routes/auth.route.js";
 import listingRouter from "./routes/listing.route.js";
 
-const port = process.env.PORT || 3000;
-
 dotenv.config();
 
-mongoose
-  .connect(process.env.MONGO_URI)
-  .then(() => console.log("Connected to MongoDB"))
-  .catch((err) => console.log(err));
-
+const port = process.env.PORT || 3000;
 const __dirname = path.resolve();
 
 const app = express();
@@ -111,7 +26,7 @@ app.use("/api/listing", listingRouter);
 
 // Serve static assets in production
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/client/dist")));
+  app.use(express.static(path.join(__dirname, "client/dist")));
   app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "client", "dist", "index.html"));
   });
@@ -131,6 +46,11 @@ app.use((err, req, res, next) => {
     statusCode,
   });
 });
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch((err) => console.log(err));
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
