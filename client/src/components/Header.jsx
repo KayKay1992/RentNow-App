a93caca2aa1fcf6eecdaaf0c2ea3dfec9335410f
KayKay1992 <kennethnwankpa1992@gmail.com>
2025-11-24 22:38:35 -0800

    fixing header avartar

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,63 +1,92 @@
-import {FaSearch} from "react-icons/fa"
-import { Link, useNavigate } from "react-router-dom"
-import { useSelector } from "react-redux"
-import { useEffect, useState } from "react"
+import { FaSearch } from "react-icons/fa";
+import { Link, useNavigate } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useEffect, useState } from "react";
 
 export default function Header() {
-
-  const {currentUser} = useSelector((state) => state.user)
-  const [searchTerm, setSearchTerm] = useState('')
-  const navigate = useNavigate()
+  const { currentUser } = useSelector((state) => state.user);
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
 
   const handleSunmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     const urlParams = new URLSearchParams(window.location.search);
-    urlParams.set('searchTerm', searchTerm);
+    urlParams.set("searchTerm", searchTerm);
     const searchQuery = urlParams.toString();
-    navigate(`/search?${searchQuery}`, {replace: true})
-  }
+    navigate(`/search?${searchQuery}`, { replace: true });
+  };
 
   //TO ENABLE THE CHANGE IN URL REPLICATE IN TH SEARCHTerm
-  useEffect(() =>{
+  useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const searchTermFromUrl = urlParams.get('searchTerm');
-    if(searchTermFromUrl){
-      setSearchTerm(searchTermFromUrl)
+    const searchTermFromUrl = urlParams.get("searchTerm");
+    if (searchTermFromUrl) {
+      setSearchTerm(searchTermFromUrl);
     }
-  }, [location.search])
+  }, [location.search]);
   return (
-    <header className='bg-slate-200 shadow-md'>
-        <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-        <Link to='/'>
-        <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
-            <span className='text-green-700'>Rent</span>
-            <span className='text-red-700'>Now</span>
-        </h1>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+            <span className="text-green-700">Rent</span>
+            <span className="text-red-700">Now</span>
+          </h1>
         </Link>
-        <form onSubmit={handleSunmit} className='bg-slate-100 p-3 rounded-lg flex items-center  '>
-            <input type='text' placeholder='Search...' className='bg-transparent focus:outline-none w-24 sm:w-64' value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-            <button>
-            <FaSearch className=" text-slate-600"/>
-            </button>   
+        <form
+          onSubmit={handleSunmit}
+          className="bg-slate-100 p-3 rounded-lg flex items-center  "
+        >
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent focus:outline-none w-24 sm:w-64"
+            value={searchTerm}
+            onChange={(e) => setSearchTerm(e.target.value)}
+          />
+          <button>
+            <FaSearch className=" text-slate-600" />
+          </button>
         </form>
         <ul className="flex gap-4">
-            <Link to='/'>
-            <li className="hidden sm:inline text-slate-700 hover:underline">Home</li>
-            </Link>
-            <Link to='/About'>
-            <li  className="hidden sm:inline text-slate-700 hover:underline">About</li>
-            </Link>
-            <Link to='/profile'>
+          <Link to="/">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              Home
+            </li>
+          </Link>
+          <Link to="/About">
+            <li className="hidden sm:inline text-slate-700 hover:underline">
+              About
+            </li>
+          </Link>
+          <li
+            className="cursor-pointer"
+            onClick={() => {
+              if (currentUser) {
+                navigate("/profile");
+              } else {
+                navigate("/signin");
+              }
+            }}
+          >
             {currentUser ? (
-           <img className="rounded-full  h-7 w-7 object-cover" src={currentUser.avatar} alt="Profile"/>
-            ) :
-                
-                <li  className="text-slate-700 hover:underline">Sign In</li>
-            
-            }
-            </Link>   
+              <img
+                className="rounded-full h-7 w-7 object-cover"
+                src={currentUser.avatar}
+                alt="Profile"
+              />
+            ) : (
+              <span
+                role="img"
+                aria-label="sign-in"
+                className="text-slate-700 hover:underline"
+              >
+                🔑
+              </span>
+            )}
+          </li>
         </ul>
-        </div>
+      </div>
     </header>
-  )
+  );
 }
